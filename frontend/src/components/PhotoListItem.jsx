a72793d92730212d3d4809ad2eea8b0a880a0e2d
Justin <justinhao@live.com>
2023-10-06 13:31:10 -0400

    Render PhotoListItem with sample data

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,20 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  const {sampleDataForPhotoListItem} = props
+  
+  return(
+    <div>
+      <img src={sampleDataForPhotoListItem.imageSource} alt="" />
+      <img src={sampleDataForPhotoListItem.profile} alt="" />
+      <p>
+        {sampleDataForPhotoListItem.username} <br/>
+        {sampleDataForPhotoListItem.location.city}, {sampleDataForPhotoListItem.location.country}
+      </p>
+    </div>
+  )
 };
 
 export default PhotoListItem;
