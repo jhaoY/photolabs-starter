69a9db164c27bb19dec4cc8bd5bec769b1f2cf1b
Justin <justinhao@live.com>
2023-10-06 14:44:58 -0400

    Add TopicList to render topics at top of page

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -8,10 +8,12 @@ const sampleDataForTopicListItem = {
   label: "Nature",
 };
 
-const TopicListItem = () => {
+const TopicListItem = (props) => {
+  const {topic} = props
+
   return (
     <div className="topic-list__item">
-      {/* Insert React */}
+      <span>{topic.title}</span>
     </div>
   );
 };
