7566969f178720619b762470c531adf35f7e2b78
Justin <justinhao@live.com>
2023-10-07 16:09:47 -0400

    Store global states in useApplicationData hook

---
+++ frontend/src/hooks/useApplicationData.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+function useApplicationData() {
+  
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [selectedPhoto, setSelectedPhoto] = useState(null)
+  const [favoritePhotos, setFavoritePhotos] = useState([])
+
+
+  const updateToFavPhotoIds = (photo) => { 
+    const isPhotoFavorited = favoritePhotos.includes(photo.id);
+    if (isPhotoFavorited) {
+      setFavoritePhotos(favoritePhotos => favoritePhotos.filter(id => id !== photo.id))
+    } else {
+      setFavoritePhotos(favoritePhotos => [...favoritePhotos, photo.id])
+    }
+  }
+
+  const setPhotoSelected = photo => {
+    setIsModalOpen(true)
+    setSelectedPhoto(photo)
+  }
+
+  const onClosePhotoDetailsModal = () => {
+    setIsModalOpen(false)
+    setSelectedPhoto(null)
+  }
+
+  return {
+    state: {
+      isModalOpen,
+      selectedPhoto,
+      favoritePhotos
+    },
+    updateToFavPhotoIds,
+    setPhotoSelected,
+    onClosePhotoDetailsModal
+  }
+}
+
+export default useApplicationData;
